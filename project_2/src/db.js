fba12caa5be66991e15039bcd8b0e958c10c6d16
moiz2405 <km856307@gmail.com>
2025-01-16 17:30:27 +0000

    Modify database setup in project_2

--- project_2/src/db.js
+++ project_2/src/db.js
@@ -1,21 +1,25 @@
-import { DatabaseSync } from 'node:sqlite'
-const db = new DatabaseSync(':memory')
+import { DatabaseSync } from 'node:sqlite';
 
+const db = new DatabaseSync(':memory');
+
+// Create users table if it doesn't already exist
 db.exec(`
-    CREATE TABLE users (
-        id INTERGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE,
         password TEXT
     )
-    `)
+`);
+
+// Create todos table if it doesn't already exist
 db.exec(`
-    CREATE TABLE todos(
-        id INTERGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS todos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER,
         task TEXT,
         completed BOOLEAN DEFAULT 0,
         FOREIGN KEY(user_id) REFERENCES users(id)
     )
-    `)
+`);
 
-export default db    
+export default db;
