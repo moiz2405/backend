64c8d207669b6daaee99a0c7602ee33624f0d26d
moiz2405 <km856307@gmail.com>
2025-01-18 16:25:57 +0000

    Modify server setup in src/server.js

--- project_2/src/server.js
+++ project_2/src/server.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 3001
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-
+import authMiddleware from './middleware/authMiddleware.js'
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../public')))
 
@@ -18,7 +18,8 @@ app.get('/', (req, res) => {
 
 //routes
 app.use('/auth', authRoutes)
-app.use('/todo', todoRoutes)
+
+app.use('/todos', authMiddleware, todoRoutes)
 
 app.listen(PORT, () => {
     console.log(`server started at ${PORT}`)
