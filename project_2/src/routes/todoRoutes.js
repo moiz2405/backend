77e282e4239f2595402cf1500cc70c76cb2e6718
moiz2405 <km856307@gmail.com>
2025-01-18 17:25:07 +0000

    Modify todoRoutes in src/routes/todoRoutes.js

--- project_2/src/routes/todoRoutes.js
+++ project_2/src/routes/todoRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.get('/', (req, res) => {
 
     const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`)
-    const todo = getTodos.all(req.userId)
+    const todos = getTodos.all(req.userId)
     res.json(todos)
 
 })
