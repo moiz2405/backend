bcd9ada9cb4d5f648fbec2014be88ac784aa3d39
moiz2405 <km856307@gmail.com>
2025-01-18 17:25:07 +0000

    Modify authentication middleware in src/middleware/authMiddleware.js

--- project_2/src/middleware/authMiddleware.js
+++ project_2/src/middleware/authMiddleware.js
@@ -1,14 +1,14 @@
 import jwt from 'jsonwebtoken'
 
-function authMiddleware(req,res,next){
+function authMiddleware(req, res, next) {
     const token = req.headers['authorization']
 
-    if(!token){return res.status(401).json({message : "No token Provided"})}
+    if (!token) { return res.status(401).json({ message: "No token Provided" }) }
 
-    jwt.verify(token,process.end.JWT_SECRET, (err, decoded)=>{
-        
-        if(err){return res.status(401).json({message:"Invalid Token"})}
-      
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+
+        if (err) { return res.status(401).json({ message: "Invalid Token" }) }
+
         req.userId = decoded.id
         next()
     })
