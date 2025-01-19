6033c384e426ed9273a42822b50f3644b1f8252f
moiz2405 <km856307@gmail.com>
2025-01-19 08:32:59 +0000

    Modify database setup in src/db.js

--- project_2/src/db.js
+++ project_2/src/db.js
@@ -1,29 +1,23 @@
-import { DatabaseSync } from 'node:sqlite';
+import { DatabaseSync } from 'node:sqlite'
+const db = new DatabaseSync(':memory:')
 
-const db = new DatabaseSync(':memory:');
-
-// Drop the tables if they already exist to reset on each restart
-// db.exec(`DROP TABLE IF EXISTS users`);
-// db.exec(`DROP TABLE IF EXISTS todos`);
-
-// Create users table
+// Execute SQL statements from strings
 db.exec(`
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE,
         password TEXT
     )
-`);
+`)
 
-// Create todos table
 db.exec(`
-    CREATE TABLE IF NOT EXISTS todos (
+    CREATE TABLE todos (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER,
         task TEXT,
         completed BOOLEAN DEFAULT 0,
         FOREIGN KEY(user_id) REFERENCES users(id)
-    )
-`);
+    )    
+`)
 
-export default db;
+export default db
