87ad81f9aab96ce6f8181aeb3863f8b51b487695
moiz2405 <km856307@gmail.com>
2025-01-19 16:11:26 +0000

    Modify todoRoutes in src/routes/todoRoutes.js

--- project_2/src/routes/todoRoutes.js
+++ project_2/src/routes/todoRoutes.js
@@ -14,6 +14,12 @@ router.get('/', (req, res) => {
 //create a new todo
 router.post('/', (req, res) => {
 
+    const { todoData } = req.body
+    console.log(todoData)
+    const newTodo = db.prepare(`INSERT INTO todos (user_id,task) VALUES (?,?)`)
+
+    newTodo.run(req.userId, todoData)
+
 })
 
 //update a todo
