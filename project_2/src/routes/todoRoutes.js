004076a06d142765b759c127e5b53ddc7bf9663f
moiz2405 <km856307@gmail.com>
2025-01-19 08:32:59 +0000

    Modify todoRoutes in src/routes/todoRoutes.js

--- project_2/src/routes/todoRoutes.js
+++ project_2/src/routes/todoRoutes.js
@@ -5,12 +5,11 @@ const router = express.Router()
 
 //get all todos for logged in users
 router.get('/', (req, res) => {
-
-    const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`)
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
     const todos = getTodos.all(req.userId)
     res.json(todos)
-
 })
+
 
 //create a new todo
 router.post('/', (req, res) => {
