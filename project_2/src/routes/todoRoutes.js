0104dbe5a6fdffa6b520c11916a26b3a7d13b4ee
moiz2405 <km856307@gmail.com>
2025-01-22 17:26:46 +0000

    Modify todoRoutes in src/routes/todoRoutes.js

--- project_2/src/routes/todoRoutes.js
+++ project_2/src/routes/todoRoutes.js
@@ -19,17 +19,30 @@ router.post('/', (req, res) => {
     const newTodo = db.prepare(`INSERT INTO todos (user_id,task) VALUES (?,?)`)
 
     newTodo.run(req.userId, todoData)
+    res.json({ message: "New Todo Created" })
 
 })
 
 //update a todo
 router.put('/:id', (req, res) => {
 
+    const { id } = req.params
+    const { completed } = req.body
+    const updateTodo = db.prepare(`UPDATE todos SET completed = (?) WHERE id = (?)`)
+
+    updateTodo.run(completed, id)
+    res.json({ message: "Todo completed" })
 })
 
 //delete a todo
 router.delete('/:id', (req, res) => {
 
+    const { id } = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? and user_Id = ?`)
+    deleteTodo.run(id, userId)
+    res.json({ message: "Todo Deleted" })
 })
+
 
 export default router;
